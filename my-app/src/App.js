b169c730948d2d8ae4ac782b763d2d5106528b93
Stockman-Jr <stockmanmimmi@gmail.com>
2023-01-19 10:49:31 +0000

    push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps />
+      <FunctionalGreetingWithProps greeting="Nice to meet ya" name="Jake" age="34" />
     </div>
   );
 }
