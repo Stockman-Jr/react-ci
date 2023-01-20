3123992b34bd60fe90c4ba0202914b6894aa3025
Stockman-Jr <stockmanmimmi@gmail.com>
2023-01-20 12:10:05 +0000

    licycles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import RenderingLists from './components/RenderingLists';
+import LifeCyclesCDU from './components/LifeCyclesCDU';
 
 
 
 function App() {
   return (
     <div className="App">
-      <RenderingLists />
+      <LifeCyclesCDU />
 
     </div>
   );
