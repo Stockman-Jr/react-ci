9c28fd8e7aa95ddf002e41c74f2c4ff983677245
Stockman-Jr <stockmanmimmi@gmail.com>
2023-01-20 08:11:15 +0000

    es7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,53 @@
+// v created by rfce
+
+//import React from 'react'
+
+//function ES7Snippets() {
+//  return (
+//    <div>ES7Snippets</div>
+//  )
+//}
+
+//export default ES7Snippets
+
+// v created by rafce
+
+//import React from 'react'
+
+//const ES7Snippets = () => {
+//  return (
+//    <div>ES7Snippets</div>
+//  )
+//}
+
+//export default ES7Snippets
+
+// v created by rce
+
+//import React, { Component } from 'react'
+
+//export class ES7Snippets extends Component {
+//  render() {
+//    return (
+//      <div>ES7Snippets</div>
+//    )
+//  }
+//}
+
+//export default ES7Snippets
+
+// v created by rconst
+
+//constructor(props) {
+//  super(props)
+
+//  this.state = {
+//     first
+//  }
+//}
+
+//clg log to console
+// console.log(first)
+
+//clo log an object along with its name as str
+//console.log('first', first)
